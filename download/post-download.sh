88b60931eec3b50d71c09b995c2b53d40d2bc64c
ErBW_s <erbws@foxmail.com>
2025-09-18 15:59:11 +0800

    remove BOM

--- download/post-download.sh
+++ download/post-download.sh
@@ -7,17 +7,17 @@ mkdir -p $DEST
 
 export OHOS_SDK=$HOME/tdlib/sdk/ohos-sdk/linux
 export OHOS_NDK_HOME=$HOME/tdlib/sdk/ohos-sdk/linux
-export PATH=${OHOS_NDK_HOME}/native/build-tools/cmake/bin:$PATH
-export AS=${OHOS_NDK_HOME}/native/llvm/bin/llvm-as
-export CC="${OHOS_NDK_HOME}/native/llvm/bin/clang --target=aarch64-linux-ohos --sysroot=${OHOS_NDK_HOME}/native/sysroot"
-export CXX="${OHOS_NDK_HOME}/native/llvm/bin/clang++ --target=aarch64-linux-ohos --sysroot=${OHOS_NDK_HOME}/native/sysroot"
-export LD=${OHOS_NDK_HOME}/native/llvm/bin/ld.lld
-export STRIP=${OHOS_NDK_HOME}/native/llvm/bin/llvm-strip
-export RANLIB=${OHOS_NDK_HOME}/native/llvm/bin/llvm-ranlib
-export OBJDUMP=${OHOS_NDK_HOME}/native/llvm/bin/llvm-objdump
-export OBJCOPY=${OHOS_NDK_HOME}/native/llvm/bin/llvm-objcopy
-export NM=${OHOS_NDK_HOME}/native/llvm/bin/llvm-nm
-export AR=${OHOS_NDK_HOME}/native/llvm/bin/llvm-ar
+export PATH=$OHOS_NDK_HOME/native/build-tools/cmake/bin:$PATH
+export AS=$OHOS_NDK_HOME/native/llvm/bin/llvm-as
+export CC="$OHOS_NDK_HOME/native/llvm/bin/clang --target=aarch64-linux-ohos --sysroot=$OHOS_NDK_HOME/native/sysroot"
+export CXX="$OHOS_NDK_HOME/native/llvm/bin/clang++ --target=aarch64-linux-ohos --sysroot=$OHOS_NDK_HOME/native/sysroot"
+export LD=$OHOS_NDK_HOME/native/llvm/bin/ld.lld
+export STRIP=$OHOS_NDK_HOME/native/llvm/bin/llvm-strip
+export RANLIB=$OHOS_NDK_HOME/native/llvm/bin/llvm-ranlib
+export OBJDUMP=$OHOS_NDK_HOME/native/llvm/bin/llvm-objdump
+export OBJCOPY=$OHOS_NDK_HOME/native/llvm/bin/llvm-objcopy
+export NM=$OHOS_NDK_HOME/native/llvm/bin/llvm-nm
+export AR=$OHOS_NDK_HOME/native/llvm/bin/llvm-ar
 export CFLAGS='-fPIC -D__MUSL__=1'
 export CXXFLAGS='-fPIC -D__MUSL__=1'
 
--- scripts/tdlib.sh
+++ scripts/tdlib.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 set -eu
 
@@ -7,10 +7,32 @@ pushd $HOME/tdlib/td
 if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
-	rm -rf .build
+	rm -rf .build-native
+	rm -rf .build-arm64
 	exit 0
 else
 	exit 1
 fi
+
+mkdir -p .build-native
+mkdir -p .build-arm64
+
+# Generate TDLib source files
+cd .build-native
+cmake -DTD_GENERATE_SOURCE_FILES=ON ..
+cmake --build .
+cd ..
+
+# Build TDLib
+cd .build-arm64
+cmake -L \
+  -DCMAKE_TOOLCHAIN_FILE=$OHOS_NDK_HOME/native/build/cmake/ohos.toolchain.cmake \
+  -DCMAKE_SKIP_RPATH=TRUE \
+  -DOPENSSL_ROOT_DIR=$DEST/openssl \
+  -DOHOS_STL=c++_static \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -GNinja \
+  ..
+cmake --build . --target tdjson
 
 popd
